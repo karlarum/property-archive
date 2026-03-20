3d8298e78b54bcd25b17bbb2681b0161cbae9a8a
karlarum <131808949+karlarum@users.noreply.github.com>
2026-03-19 22:31:38 -0500

    deploying to render

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true
 }));
 
