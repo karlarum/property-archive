c79b09bc5c88349a9cdce7a09425f5ed2ad399b5
karlarum <131808949+karlarum@users.noreply.github.com>
2026-03-28 13:03:40 -0500

    fixing bugs

--- server/src/server.ts
+++ server/src/server.ts
@@ -34,7 +34,8 @@ app.use(session({
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 // 24 hours
+    maxAge: 1000 * 60 * 60 * 24, // 24 hours
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   }
 }));
 
