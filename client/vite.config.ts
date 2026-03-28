2485acc42c1d605f8d870049c2ea6c911f0cadf8
karlarum <131808949+karlarum@users.noreply.github.com>
2026-03-28 12:46:31 -0500

    vite

---
+++ client/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        'item-detail': resolve(__dirname, 'item-detail.html'),
+      }
+    }
+  }
+})
